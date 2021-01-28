2776794896e25f2c7b54a916080cdea633a3c8d8
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2021-01-28 17:38:20 +0100

    Add files via upload

--- script/scroll2head.js
+++ script/scroll2head.js
@@ -11,9 +11,10 @@ for (i=0; i < theaders.length; i++) {
 
 //FOR ALL HEADERS
 
-var allheaders = document.querySelectorAll("body *");
-for (i=0; i < allheaders.length; i++) {
-	if(allheaders[i].tagName == "H1"||"H2"||"H3"||"H4"||"H5"||"H6"){
-	   allheaders[i].addEventListener('click', tfunction);
+var allBodyElements = document.querySelectorAll("body *");
+for (i=0; i < allBodyElements.length; i++) {
+	if(allBodyElements[i].tagName == ("H1"||"H2"||"H3"||"H4"||"H5"||"H6")){
+	   var aHeadingElement = allBodyElements[i];
+		aHeadingElement.addEventListener('click', tfunction);
 	   }
 };
