2e7198e60d7ef4f7affde98ce9c71d24c134ff91
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2021-07-27 14:17:43 +0100

    Add files via upload

--- script/BNodesG/bibleNodes-7.js
+++ script/BNodesG/bibleNodes-7.js
@@ -299,3 +299,21 @@ function selectedMouseUp(ev) {
     
     document.removeEventListener('mousemove', selectedMouseMove);
 }
+
+/* SEARCH BIBLE WEBSITE SECTION NAV MENU*/
+function searchNavMenu(sbox, list2search) {
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById(sbox) || sbox;
+	filter = input.value.toUpperCase();
+	ul = document.getElementById(list2search);
+	li = ul.getElementsByTagName("li");
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
