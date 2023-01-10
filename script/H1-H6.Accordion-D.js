2495628e184d2d89aa9e856ef05d35d7afdabdd6
SammyLight <59570031+SammyLight@users.noreply.github.com>
2023-01-10 20:32:54 +0100

    Add files via upload

--- script/H1-H6.Accordion-D.js
+++ script/H1-H6.Accordion-D.js
@@ -1,29 +1,51 @@
 /*HIDE ALL ELEMENTS THAT ARE NOT H1 (or H2)*/
 let b = document.querySelectorAll("body > *");
 
-var htmlhArray = ["H6", "H5", "H4", "H3", "H2", "H1"]; 
- 
+var htmlhArray = ["H6", "H5", "H4", "H3", "H2", "H1"];
+var displayNoneOrBlock = '';
+function hideOrShowAllHnum(){
+	htmlhArray.forEach(element => {
+		let h2showit = "." + element;
+		if(displayNoneOrBlock=='none'){
+			document.querySelectorAll(htmlhArray.map(x=>'.'+x).toString()).forEach(sh=>{
+				console.log(displayNoneOrBlock)
+				sh.style.display = displayNoneOrBlock;
+				// let h2showit = sh.classList.toString().match(/\bH[1-6]\b/) + "2showit";
+				// sh.classList.add(h2showit);
+			})
+		} else {
+			document.querySelectorAll(h2showit).forEach(sh=>{
+				console.log(displayNoneOrBlock)
+				sh.style.display = displayNoneOrBlock;
+				// sh.classList.remove(h2showit);
+			})
+		}
+	});
+	if(displayNoneOrBlock=='none'){displayNoneOrBlock='';}
+	else {displayNoneOrBlock='none';}
+}
+
 /* To ensure it starts hiding element after the first H1 and not from before it, if there is another element before it */
 for (k = 0; k < b.length; k++) {
 	if(b[k].tagName == "H1"){
 		var i = k;
 		break;
-	   }
+	}
 };
- 
+
 /* Add classes indicating the header elements are under */
 
 for (i; i < b.length; i++) {
 	var num = b[i].tagName.charAt(1);
 	var a = num;
-
-	var btag = b[i].tagName;
+	
+	var btag = b[i].tagName.toUpperCase();
 	var showaar;
-
+	
 	/* For H1 to H6 */
 	if (htmlhArray.includes(btag)) {
-		var showaar = btag;
-
+		showaar = btag;
+		
 		/* Add showaar classes to Headers */
 		var x = a - 1;
 		var showaarH = "H" + x;
@@ -40,13 +62,12 @@ for (i; i < b.length; i++) {
 	//	bhSpan.style.color = "blue";
 	} 
 	/* For Elements other than H1 - H6 and SCRIPT */
-	else if (btag != "SCRIPT") {
-		b[i].classList.add(showaar);
-	}
-
 	/* HIDE ALL ELEMENTS THAT ARE NOT H1 */
+
 	if ((btag != "H1") && (btag != "SCRIPT")) {
 		b[i].style.display = "none";
+		var h2showit = showaar
+		if (!htmlhArray.includes(btag)) {b[i].classList.add(h2showit);}
 	}
 };
 
@@ -68,14 +89,10 @@ function togglefunction() {
 
 	var hSib1 = this.nextElementSibling;
 	var hSibs = this.nextElementSibling;
-	var num = this.tagName.charAt(1);
-
-	var hSibstag = hSibs.tagName;
-	var hSibstagIndex = htmlhArray.indexOf(hSibstag);
 
 	var htag = this.tagName;
-	var htagIndex = htmlhArray.indexOf(htag);
-	var h2showit = htag + "2showit";
+	// var htagIndex = htmlhArray.indexOf(htag);
+	var h2showit = htag;
 
 	/* Check the state of the nextElementSibling to determine whether to hide or to show */
 	if (hSib1.style.display == "none") {
@@ -93,14 +110,14 @@ function togglefunction() {
 		//Hide
 		if (state == "none") {
 			if (hSibs.style.display != "none") {
-				hSibs.classList.add(h2showit);
+				// hSibs.classList.add(h2showit);
 			};
 			hSibs.style.display = "none";
 
 			/* Show (show elements the clicked header is to show it) */
 		} else if (hSibs.classList.contains(h2showit)) {
 			hSibs.style.display = "";
-			hSibs.classList.remove(h2showit);
+			// hSibs.classList.remove(h2showit);
 
 			/* Show (show elements that have the tag of the clicked header as a class) */
 		} else if (hSibs.classList.contains(htag) == true) {
@@ -111,3 +128,4 @@ function togglefunction() {
 	}
 
 }
+
