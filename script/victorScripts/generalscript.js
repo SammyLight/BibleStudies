8b10ff64650652a7d0891cd1328083de362a5d32
SammyLight <59570031+SammyLight@users.noreply.github.com>
2023-10-18 14:08:02 +0100

    Add files via upload

--- script/victorScripts/generalscript.js
+++ script/victorScripts/generalscript.js
@@ -11,20 +11,39 @@ getTablesArray.forEach(element => {
   createElemApTable.appendChild(element);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Get all <ul> elements with the class "reducedLength"
-  var uls = document.getElementsByClassName("reducedLength");
-  // Specify the start index from which you want to remove elements
-  var startIndex = 6;
-  // Loop through each <ul> element and remove items from the startIndex
-  for (var j = 0; j < uls.length; j++) {
-    var ul = uls[j];
-    // Check if the startIndex is valid
-    if (startIndex >= 0 && startIndex < ul.children.length) {
-      // Remove all <li> elements from the startIndex to the end
-      for (var i = ul.children.length - 1; i >= startIndex; i--) {
-        ul.removeChild(ul.children[i]);
-      }
-    }
+// Remove the the last element in the archive page
+const archiveElements = document.getElementsByClassName('archive');
+// Ensure there are elements with the class "archive" before attempting to remove.
+if (archiveElements.length > 0) {
+  const lastArchiveElement = archiveElements[archiveElements.length - 1];
+  const lastChild = lastArchiveElement.lastElementChild;
+  if (lastChild) {
+    // Check if there is a last child element to remove.
+    lastArchiveElement.removeChild(lastChild);
   }
-});
+}
+//Reverse the arrangement of summary categories (could not achive this Jekyll at this time)
+const categories = document.getElementsByClassName('postCategories')[0];
+const categoriesChildrenToRev = Array.from(categories.getElementsByClassName('homeResources-grid-containter'));
+categoriesChildrenToRev.reverse();
+for (const homeResourcesGridContainter of categoriesChildrenToRev) {
+  categories.appendChild(homeResourcesGridContainter);
+}
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Get all <ul> elements with the class "reducedLength"
+//   var uls = document.getElementsByClassName("reducedLength");
+//   // Specify the start index from which you want to remove elements
+//   var startIndex = 6;
+//   // Loop through each <ul> element and remove items from the startIndex
+//   for (var j = 0; j < uls.length; j++) {
+//     var ul = uls[j];
+//     // Check if the startIndex is valid
+//     if (startIndex >= 0 && startIndex < ul.children.length) {
+//       // Remove all <li> elements from the startIndex to the end
+//       for (var i = ul.children.length - 1; i >= startIndex; i--) {
+//         ul.removeChild(ul.children[i]);
+//       }
+//     }
+//   }
+// });
