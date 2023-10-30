a82c943db8b628f3b80dad7cc9325bebc6792e15
SammyLight <59570031+SammyLight@users.noreply.github.com>
2023-10-30 12:50:21 +0100

    Add files via upload

--- script/victorScripts/videos.js
+++ script/victorScripts/videos.js
@@ -1,21 +1,21 @@
+const firstBtn = document.querySelector(".videos-header-btns-cont button").classList.add('active');
+const firstTab = document.querySelector(".videotab-content").classList.add('videotab-content--active');
 const allBtns = document.querySelectorAll(".videos-header-btns-cont button");
 const allTabs = document.querySelectorAll(".videotab-content");
 
 allBtns.forEach((elem) => {
   elem.addEventListener('click', function() {
     const linkId = elem.id;
-    const titleBtnClick = elem.title;
 
     allBtns.forEach((button) => {
-      if (button.title == titleBtnClick){
+      if (button.id === linkId) {
         button.classList.add("active");
       } else {
         button.classList.remove('active');
       }
     });
-
     allTabs.forEach((tab) => {
-      if (tab.id.includes(linkId)) {
+      if (tab.id === linkId + "-content") {
         tab.classList.add("videotab-content--active");  
       } else {
         tab.classList.remove('videotab-content--active');
