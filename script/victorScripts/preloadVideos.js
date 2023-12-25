e546bf68d0b9e010e419647db8c94df2bf5556c4
SammyLight <59570031+SammyLight@users.noreply.github.com>
2023-12-25 13:51:07 +0100

    Add files via upload

--- script/victorScripts/preloadVideos.js
+++ script/victorScripts/preloadVideos.js
@@ -1,6 +1,6 @@
 setTimeout(function() {
   $('.loader-bg').fadeToggle();
-}, 1500);
+}, 2400);
 //Remove the - in Articles and Summaries Categories name
 const artSumCatNameElements = document.querySelectorAll('.ArtSumCatName');
 artSumCatNameElements.forEach(element => {
