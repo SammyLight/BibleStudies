b5e60aa358355d5f6c66b0bb55ec418b6e231cdc
SammyLight <59570031+SammyLight@users.noreply.github.com>
2024-01-12 14:43:18 +0100

    Update searchVideos.js

--- script/victorScripts/searchVideos.js
+++ script/victorScripts/searchVideos.js
@@ -2,7 +2,6 @@ const videoSearchInput = document.getElementById("videoSearch");
 const searchResultsContainer = document.getElementById("searchResults");
 const searchFromTab1Content = document.querySelector('#tab1-content');
 const displayedVideos = new Set();
-let debounceTimer;
 let observer;
 // Event listener for highlighting
 videoSearchInput.addEventListener("input", () => {
@@ -13,6 +12,17 @@ videoSearchInput.addEventListener("input", () => {
 videoSearchInput.addEventListener("input", () => {
     handleVideoSearch();
 });
+function getAttributes(video) {
+    let videoTitleAttribute = video.getAttribute("video-title");
+    let videoDateAttribute = video.getAttribute("date-posted");
+    videoDateAttribute = videoDateAttribute ? videoDateAttribute + '.' : '';
+    return { videoTitleAttribute, videoDateAttribute };
+}
+function highlightText(text, searchQuery) {
+    const escapedSearchQuery = searchQuery.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const regex = new RegExp(escapedSearchQuery, 'gi');
+    return text.replace(regex, (match) => `<span class="highlighted">${match}</span>`);
+}
 // Function to handle video highlight
 function handleHighLight(searchQuery) {
     // Disconnect the observer if it's already connected
@@ -22,78 +32,20 @@ function handleHighLight(searchQuery) {
     const videoBoxes = searchResultsContainer.querySelectorAll(".video-box");
     videoBoxes.forEach((video) => {
         const videoTitleElement = video.querySelector(".video-title");
-        var modifiedText;
-        var videoDate;
-        // Extract the date from the video title
-        const videoTitleText = videoTitleElement.textContent;
-        // Regular expression for the first date format: "23rd Dec. 2023"
-        const firstDateFormatMatch = videoTitleText.match(/(\d{1,2}(?:st|nd|rd|th)?)\s?(\w{3})\.\s?(\d{4})\.?/);
-        // Regular expression for the second date format: "Sun, Dec 24, 2023."
-        const secondDateFormatMatch = videoTitleText.match(/(?:\w{3},\s)?(\w{3})\s(\d{1,2}),\s(\d{4})\.?/);
-        // Check if a date match is found
-        if (firstDateFormatMatch && firstDateFormatMatch.length > 0) {
-            const [dayMatch, monthMatch, yearMatch] = firstDateFormatMatch.slice(1);
-            const day = dayMatch || '';
-            const month = monthMatch || '';
-            const year = yearMatch || '';
-            // Manually construct the date string in a format recognized by the Date constructor
-            const dateString = `${month} ${day.replace(/\D/g, '')}, ${year}`;
-            const dateObject = new Date(dateString);
-            // Check if the dateObject is valid
-            if (!isNaN(dateObject)) {
-                const dayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(dateObject);
-                // Create a new text node with the modified text (excluding the date)
-                modifiedText = document.createTextNode(videoTitleText.replace(secondDateFormatMatch[0], ''));
-                // Create a new text node with the date (excluding the modified text)
-                videoDate = document.createTextNode(dayOfWeek + ', ' + dateString + '.');
-            } else {
-                console.error('Invalid date object:', dateObject);
-            }
-        } else if (secondDateFormatMatch && secondDateFormatMatch.length > 0) {
-            const [, monthMatch, dayMatch, yearMatch] = secondDateFormatMatch;
-            const day = dayMatch || '';
-            const month = monthMatch || '';
-            const year = yearMatch || '';
-            const dateString = `${month} ${day}, ${year}`;
-            const dateObject = new Date(dateString);
-            // Check if the dateObject is valid
-            if (!isNaN(dateObject)) {
-                const dayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(dateObject);
-                // Create a new text node with the modified text (excluding the date)
-                modifiedText = document.createTextNode(videoTitleText.replace(secondDateFormatMatch[0], ''));
-                // Create a new text node with the date (excluding the modified text)
-                videoDate = document.createTextNode(dayOfWeek + ', ' + dateString + '.');
-            } else {
-                console.error('Invalid date object:', dateObject);
-            }
-        } else {
-            // No match for either format
-            console.error('No date match found');
-        }
-        if (videoTitleElement) {
-            // Use modifiedText instead of originalVideo
-            const escapedSearchQuery = searchQuery.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-            const regex = new RegExp(escapedSearchQuery, 'gi');
-        
-            const highlightedTitle = modifiedText.textContent.replace(
-                regex,
-                (match) => `<span class="highlighted">${match}</span>`
-            );
-            // Clear the existing content in videoTitleElement
-            videoTitleElement.innerHTML = '';
-            // Append the highlightedTitle and videoDate to videoTitleElement
-            videoTitleElement.innerHTML = highlightedTitle;
-            // Highlight the date and append it to videoTitleElement
-            if (videoDate) {
-                const highlightedDate = videoDate.textContent.replace(
-                    regex,
-                    (match) => `<span class="highlighted">${match}</span>`
-                );
+        const { videoTitleAttribute, videoDateAttribute } = getAttributes(video);
+        if (videoTitleAttribute) {
+            const highlightedTitle = highlightText(videoTitleAttribute, searchQuery);
+            const tempElement = document.createElement('div');
+            tempElement.innerHTML = highlightedTitle;
+            if (videoDateAttribute) {
+                const highlightedDate = highlightText(videoDateAttribute, searchQuery);
                 const highlightedDateElement = document.createElement('span');
                 highlightedDateElement.classList.add('video-date');
                 highlightedDateElement.innerHTML = highlightedDate;
-                videoTitleElement.appendChild(highlightedDateElement);
+
+                tempElement.innerHTML += highlightedDateElement.outerHTML;
             }
+            videoTitleElement.innerHTML = tempElement.innerHTML;
         }
     });
     // Create the observer if it doesn't exist
@@ -112,13 +64,14 @@ function handleVideoSearch() {
     displayedVideos.clear();
     const videoBoxes = searchFromTab1Content.querySelectorAll(".video-box");
     videoBoxes.forEach((video) => {
-        const videoTitleElement = video.getAttribute("video-title");
+        const { videoTitleAttribute, videoDateAttribute } = getAttributes(video);
         const videoIdElement = video.querySelector("lite-youtube");
         const videoId = videoIdElement.getAttribute("videoid");
-        if (videoTitleElement) {
-            const originalVideoTitle = videoTitleElement;
+        if (videoTitleAttribute || videoDateAttribute) {
+            const originalVideoTitle = videoTitleAttribute;
             const videoTitle = originalVideoTitle.toLowerCase();
-            if (videoTitle.includes(searchQuery) && !displayedVideos.has(videoId)) {
+            const videoDate = videoDateAttribute.toLowerCase();
+            if ((videoTitle.includes(searchQuery) || videoDate.includes(searchQuery)) && !displayedVideos.has(videoId)) {
                 const videoClone = video.cloneNode(true);
                 searchResultsContainer.appendChild(videoClone);
                 displayedVideos.add(videoId);
@@ -138,7 +91,7 @@ function handleVideoSearch() {
     searchResultsContainer.prepend(displayedVideoCountElement);
     searchResultsContainer.style.paddingTop = displayedVideoCount === 0 ? '30px' : '40px';
     if (searchQuery === "") {
-        searchResultsContainer.style.paddingTop = '0';
+        searchResultsContainer.style.removeProperty('padding-top');
         searchResultsContainer.innerHTML = "";
         displayedVideos.clear();
     }
