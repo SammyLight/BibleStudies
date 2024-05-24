bdec241b4489763a4613318adc555caef65ef2e1
SammyLight <59570031+SammyLight@users.noreply.github.com>
2024-05-24 11:13:23 +0100

    Delete _site directory

--- _site/ActiveTabGet/background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab){
-    let msg = {
-        txt: "hello"
-    };
-    chrome.tabs.sendMessage(tab.id, msg);
-}
--- _site/myCode Test 2/scripts/headerLinkToggleJS.js
+++
@@ -1,5 +0,0 @@
-const chk = document.getElementById('toggleAllBtn');
-
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
