3af3636a5f187a31e6ec1587774b106b553f99f1
SammyLight <59570031+SammyLight@users.noreply.github.com>
2024-05-24 11:34:07 +0100

    Add files via upload

--- script/victorScripts/getVideoIdsJekyll.js
+++ script/victorScripts/getVideoIdsJekyll.js
@@ -1,10 +1,29 @@
+// fetch('{{ site.baseurl }}/videoIds.json')
+// .then(response => response.json())
+// .then(videoIds => {
+//   const videoIdsList = document.getElementById('videoIdsList');
+//   videoIds.forEach(videoId => {
+//     const listItem = document.createElement('li');
+//     listItem.textContent = videoId;
+//     videoIdsList.appendChild(listItem);
+//   });
+// });
+
 fetch('{{ site.baseurl }}/videoIds.json')
-.then(response => response.json())
-.then(videoIds => {
-  const videoIdsList = document.getElementById('videoIdsList');
-  videoIds.forEach(videoId => {
-    const listItem = document.createElement('li');
-    listItem.textContent = videoId;
-    videoIdsList.appendChild(listItem);
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(videoIds => {
+    const videoIdsList = document.getElementById('videoIdsList');
+    videoIds.forEach(videoId => {
+      const listItem = document.createElement('li');
+      listItem.textContent = videoId;
+      videoIdsList.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching videoIds:', error);
   });
-});
